6ab6191f34a8d879cb0e656d27b964daf15ef2f6
ornops <joshuaornopia@gmail.com>
2024-11-06 14:27:59 +0800

    Added fetching function to fetch data from API

--- src/components/main-panel/main-panel.tsx
+++ src/components/main-panel/main-panel.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './main-panel-styles.scss'
-import Button from '../button/button'
+
 
 const MainPanel = () => {
+	const [quote, setQuote] = useState('Click button to get Quotes')
+	const [error, setError] = useState(null)
+
+
+
+	const getQuote = () => {
+		fetch('https://api.api-ninjas.com/v1/quotes', {
+			headers: {
+				'X-Api-Key': 'PVUe+B5O8BaZJs8NJf/Adw==IhfcxFJrOKpQAl3K'
+			}
+		})
+			.then(response => response.json())
+			.then(resp => {
+				if (resp.length > 0) {
+					setQuote(resp[0].quote)
+				} else {
+					setQuote('No Quotes Available')
+				}
+			})
+			.catch(err => setError(err.message))
+	}
 	return (
 		<div className='main-panel-container'>
-			Quote
-			<Button text="New Quote" />
+			{error ? <p>Error: {error}</p> : <p>{quote}</p>}
+			<button onClick={getQuote}>New Quote</button>
 		</div>
 	)
 }
